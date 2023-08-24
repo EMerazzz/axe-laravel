a98710f273af6548c42a2b614973971bd75ad0cc
EMerazzz <erlyn.meraz@unah.hn>
2023-08-24 00:18:17 -0600

    CAMBIO CONTRA

--- resources/views/AXE/cambioContrasena.blade.php
+++ resources/views/AXE/cambioContrasena.blade.php
@@ -15,6 +15,35 @@
     <!-- Agrega las hojas de estilo de Bootstrap -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- <link rel="stylesheet" href="{{ asset('css/custom.css') }}">-->
+
+    <style>
+        /* Estilos para el botón de recuperar contraseña */
+        .recovery-button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #3498db;
+            color: white;
+            text-decoration: none;
+            border: none;
+            border-radius: 5px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        
+        .recovery-button:hover {
+            background-color: #2980b9;
+        }
+    </style>
+    <style>
+    /* Estilos para el botón de abrir el modal */
+    .open-modal-button {
+        padding: 10px 20px;
+        font-size: 16px;
+    }
+</style>
+
+
+
 </head>
 <body>
     <div class="main">
@@ -26,16 +55,26 @@
                             @csrf
                             <fieldset class="clearfix">
                             <h3 class="login-heading" style="color: white;">Restablecer contraseña</h3> 
-                                <p><span class="fa fa-user"></span><input id="username-input" type="text" placeholder="Nombre de usuario" name="USUARIO" required></p>
-                                <p><span class="fa fa-lock"></span><input id="password-input" type="password" placeholder="Contraseña" name="CONTRASENA" required></p>
-                                <!-- Agrega el enlace de recuperación de contraseña -->
-                                <p><a href="#" id="forgot-password-link">¿Olvidaste tu contraseña?</a></p>
-                                <div class="buttons-container d-flex justify-content-between align-items-center">
-            
-                                    <div>
-                                        <button type="submit" class="btn btn-primary custom-btn">Iniciar sesión</button>
-                                    </div>
-                                </div>
+
+                                
+                            <div class="form-group">
+    <label for="pregunta1Respuesta">Respuesta a la pregunta 1:</label>
+    <input type="text" class="form-control" id="pregunta1Respuesta"style="width: 200px; height: 30px;">
+</div>
+
+<div class="form-group">
+    <label for="pregunta2Respuesta">Respuesta a la pregunta 2:</label>
+    <input type="text" class="form-control" id="pregunta2Respuesta"style="width: 200px; height: 30px;">
+</div>
+
+                        <div class="form-group">
+                            <button type="button" class="btn btn-primary" id="checkAnswersButton">Verificar respuestas</button>
+                        </div>
+                            
+                        <div>
+                            <button type="button" class="open-modal-button btn btn-primary" id="openModalButton" disabled>Recuperar contraseña</button>
+                        </div>
+
 
                                 @if(session('errorMessage'))
                                     <div  class="text-white font-weight-bold">
@@ -57,8 +96,8 @@
 
 
    <!-- Modal de Recuperación de Contraseña -->
-<!-- Modal de Recuperación de Contraseña -->
-<div class="modal fade" id="forgotPasswordModal" tabindex="-1">
+    <!-- Modal de Recuperación de Contraseña -->
+    <div class="modal fade" id="forgotPasswordModal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
@@ -85,7 +124,6 @@
             </div>
         </div>
     </div>
-
 
 <script>
     const newPasswordInput = document.getElementById("newPassword");
@@ -119,5 +157,50 @@
     document.getElementById("forgot-password-link").style.color = "white"; // Cambia el color a rojo
 </script>
 
+
+<script>
+    // ... Tu código actual ...
+
+    // Define una variable para controlar si se respondieron las preguntas
+    let answeredQuestions = false;
+
+    // Función para habilitar el botón si se respondieron las preguntas
+    function enableRecoveryButton() {
+        answeredQuestions = true;
+        document.getElementById("openModalButton").removeAttribute("disabled");
+    }
+
+    // Escucha las respuestas de las preguntas y habilita el botón si se respondieron
+    document.getElementById("pregunta1Respuesta").addEventListener("change", function() {
+        // Lógica para verificar respuesta
+        if (/* Condición de respuesta correcta */) {
+            enableRecoveryButton();
+        }
+    });
+
+    document.getElementById("pregunta2Respuesta").addEventListener("change", function() {
+        // Lógica para verificar respuesta
+        if (/* Condición de respuesta correcta */) {
+            enableRecoveryButton();
+        }
+    });
+
+    // ... Tu código actual ...
+</script>
+
+<script>
+    // ... Tu código actual ...
+
+    // Manejar el clic en el enlace "¿Olvidaste tu contraseña?"
+    document.getElementById("openModalButton").addEventListener("click", function() {
+        if (answeredQuestions) {
+            $('#forgotPasswordModal').modal('show');
+        } else {
+            // Mostrar mensaje o realizar alguna acción si no se respondieron las preguntas
+        }
+    });
+
+    // ... Tu código actual ...
+</script>
 </body>
 </html>
