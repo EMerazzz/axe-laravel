9d4660069e3d2ed7cd7182c1890bbd3008ceb5c2
Milton Amador <miltonamador1508@gmail.com>
2023-11-17 13:19:05 -0600

    Funcionalidad de preguntas

--- app/Http/Controllers/AXE/preguntasController.php
+++ app/Http/Controllers/AXE/preguntasController.php
@@ -35,7 +35,7 @@ class preguntasController extends Controller
         $nueva_preguntas = Http::withHeaders([
             'Authorization' => 'Bearer ' . $token,
         ])->post($this->apiUrl,[
-        "NUEVA_PREGUNTA" => $request->input("NUEVA_PREGUNTA"),
+        "PREGUNTA" => $request->input("PREGUNTA"),
         ]);
          // Verificar si la solicitud fue exitosa y redireccionar con mensaje de éxito o error
          if ($nueva_preguntas ->successful()) {
@@ -58,7 +58,7 @@ class preguntasController extends Controller
             'Authorization' => 'Bearer ' . $token,
         ])->put($this->apiUrl.'/'.$request->input("COD_PREGUNTA"),[
         "COD_PREGUNTA"=> $request->input("COD_PREGUNTA"),
-        "NUEVA_PREGUNTA" => $request->input("NUEVA_PREGUNTA"),
+        "PREGUNTA" => $request->input("PREGUNTA"),
         ]);
        //print_r([$putformatos]);die();
 
