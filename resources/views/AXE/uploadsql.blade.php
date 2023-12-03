2730d0a6fcbf0c09d8fa4f76d4e4903fefcb348b
EMerazzz <erlyn.meraz@unah.hn>
2023-12-02 23:24:53 -0600

    blade upload

--- resources/views/AXE/uploadsql.blade.php
+++ resources/views/AXE/uploadsql.blade.php
@@ -2,12 +2,24 @@
 
 @section('title', 'Restore')
 
+@section('content_header')
+    <style>
+        .custom-blockquote {
+            line-height: 0;
+            margin-top: -5px;
+            margin-bottom: -5px;
+        }
+    </style>
+    <blockquote class="custom-blockquote">
+        <p class="mb-0">Subir archivo SQL para restaurar la base de datos</p>
+    </blockquote>
+@stop
 @section('content')
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <div class="container">
-        <h2>Subir archivo SQL para restaurar la base de datos</h2>
+        
 
         @if(session('notification'))
             <div class="alert alert-{{ session('notification.type') }}">
@@ -22,17 +34,40 @@
         @endif
 
         <form action="{{ url('backuprestore/restaurar') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label for="backupFile">Seleccionar archivo .SQL:</label>
-                <input type="file" name="backupFile" class="form-control" accept=".sql" required>
-            </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-primary">Restaurar</button>
-                <a href="{{ url('backuprestore') }}" class="btn btn-danger">Cancelar</a>
-            </div>
-        </form>
+    @csrf
+    <div class="form-group">
+        <label for="backupFile" class="form-label">Seleccionar archivo .SQL:</label>
+        <input type="file" name="backupFile" class="form-control" accept=".sql" required>
     </div>
+
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Restaurar</button>
+        <a href="{{ url('backuprestore') }}" class="btn btn-danger">Cancelar</a>
+    </div>
+</form>
+<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
   
 
-@endsection
+@stop
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.6/b-2.4.1/b-html5-2.4.1/b-print-2.4.1/datatables.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon"/>
+@stop
+@section('js')
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap5.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-1.13.6/b-2.4.1/b-html5-2.4.1/b-print-2.4.1/datatables.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js"></script>
+ 
+
+
+@stop
